ab6dd4993a1002edb58ccb4adfc29c273e1fe6b0
egorschetinyuk <chepolette@yandex.ru>
2024-07-29 18:54:59 +0300

    Update num 1.py

--- num 1.py
+++ num 1.py
@@ -4,23 +4,32 @@ try:
         grades = []
         for line in file:
             city, grade = line.strip().split(":")
-            grades.append(grade)
+            grades.append(int(grade))  
             cities.append(city)
             print(city)
             print(grade)
+    
+ 
     slovar = dict(zip(cities, grades))
     print(slovar)
-    nagmi = input("выберите 1,чтобы увидеть мин.колво.жит  ")
-    minimum = []
+    
+  
+    nagmi = input("Выберите 1, чтобы увидеть мин. количество жителей: ")
+    
+   
     min_gorod = min(slovar.values())
     min_keys = [i for i, value in slovar.items() if value == min_gorod]
-    minimum.append(min_keys)
-    print(minimum)
-    sortirovochka = {key: value for key, value in slovar.items() if value > min(slovar.values())}
-    print(sortirovochka)
-    sort_sortirovochka = {key:value for key, value in sorted(sortirovochka.items())}
-    with open(' filtered_cities.txt', 'w') as file:
+    print(f'Города с минимальным количеством жителей: {min_keys}')
+    
+ 
+    sortirovochka = {key: value for key, value in slovar.items() if value > min_gorod}
+    print(f'Отфильтрованный словарь: {sortirovochka}')
+    
+    sort_sortirovochka = dict(sorted(sortirovochka.items()))
+    
+
+    with open('filtered_cities.txt', 'w') as file:
         for key, value in sort_sortirovochka.items():
             file.write(f'{key}:{value}\n')
 except FileNotFoundError:
-    print("файль не найден")
+    print("Файл не найден")
